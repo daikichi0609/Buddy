e0ec173440fc967fdbe124adf8df5d6483966749
東大貴 <nisisizema@icloud.com>
2023-05-04 00:31:36 +0900

    ボスステージ作成

---
+++ Assets/Script/Dungeon/Setup/BossBattleSetup.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Dungeon/BossBattle")]
+public class BossBattleSetup : ScriptableObject
+{
+    /// <summary>
+    /// ステージ名
+    /// </summary>
+    [SerializeField, Header("ステージ名")]
+    [ResizableTextArea]
+    private string m_BossBattleName;
+    public string BossBattleName => m_BossBattleName;
+
+    /// <summary>
+    /// ステージ
+    /// </summary>
+    [SerializeField, Header("ステージ")]
+    private GameObject m_Stage;
+    public GameObject Stage => m_Stage;
+
+    /// <summary>
+    /// 到着時会話フロー
+    /// </summary>
+    [SerializeField, Header("到着時会話フロー")]
+    private GameObject m_ArrivalFlow;
+    public GameObject ArrivalFlow => m_ArrivalFlow;
+
+    /// <summary>
+    /// 撃破時会話フロー
+    /// </summary>
+    [SerializeField, Header("撃破時会話フロー")]
+    private GameObject m_DefeatFlow;
+    public GameObject DefeatFlow => m_DefeatFlow;
+}
--- Assets/Script/Dungeon/Setup/DungeonSetupHolder.cs
+++ Assets/Script/Dungeon/Setup/DungeonSetupHolder.cs
@@ -38,4 +38,12 @@ public class DungeonSetupHolder : ScriptableObject
     [Header("アイテム設定")]
     private ItemDeploySetup m_ItemDeploySetup;
     public ItemDeploySetup ItemDeploySetup => m_ItemDeploySetup;
+
+    /// <summary>
+    /// ボスバトル設定
+    /// </summary>
+    [SerializeField, Expandable]
+    [Header("ボスバトル設定")]
+    private BossBattleSetup m_BossBattleSetup;
+    public BossBattleSetup BossBattleSetup => m_BossBattleSetup;
 }
