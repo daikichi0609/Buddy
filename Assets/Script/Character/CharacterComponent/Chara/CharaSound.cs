ca0dcf9c13c195aad207d072f6b2d4a3012f8e3a
東大貴 <nisisizema@icloud.com>
2023-05-03 21:02:19 +0900

    サウンドホルダー

--- Assets/Script/Character/CharacterComponent/Chara/CharaSound.cs
+++ Assets/Script/Character/CharacterComponent/Chara/CharaSound.cs
@@ -10,23 +10,6 @@ public interface ICharaSound : IActorInterface
 
 public class CharaSound : ActorComponentBase, ICharaSound
 {
-    private static bool ms_IsSet = false;
-
-    /// <summary>
-    /// 攻撃
-    /// </summary>
-    private static AudioSource ms_AttackSound;
-
-    /// <summary>
-    /// 空振り
-    /// </summary>
-    private static AudioSource ms_MissSound;
-
-    /// <summary>
-    /// 被ダメージ
-    /// </summary>
-    private static AudioSource ms_DamageSound;
-
     protected override void Register(ICollector owner)
     {
         base.Register(owner);
@@ -37,15 +20,6 @@ public class CharaSound : ActorComponentBase, ICharaSound
     {
         base.Initialize();
 
-        if (ms_IsSet == false)
-        {
-            ms_IsSet = true;
-            var attack = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.AttackSound);
-            ms_AttackSound = attack.GetComponent<AudioSource>();
-            ms_MissSound = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.MissSound).GetComponent<AudioSource>();
-            ms_DamageSound = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.DamageSound).GetComponent<AudioSource>();
-        }
-
         if (Owner.RequireEvent<ICharaBattleEvent>(out var battle) == true)
         {
             battle.OnAttackStart.Subscribe(_ =>
@@ -53,7 +27,7 @@ public class CharaSound : ActorComponentBase, ICharaSound
                 // 攻撃音
                 StartCoroutine(Coroutine.DelayCoroutine(CharaBattle.ms_NormalAttackHitTime, () =>
                 {
-                    ms_AttackSound.Play();
+                    SoundHolder.Interface.AttackSound.Play();
                 }));
             }).AddTo(CompositeDisposable);
 
@@ -62,14 +36,14 @@ public class CharaSound : ActorComponentBase, ICharaSound
                 if (result.IsHit == false)
                 {
                     // 空振り音
-                    ms_MissSound.Play();
+                    SoundHolder.Interface.MissSound.Play();
                 }
             }).AddTo(CompositeDisposable);
 
             battle.OnDamageEnd.Subscribe(_ =>
             {
                 // ダメージ音
-                ms_DamageSound.Play();
+                SoundHolder.Interface.DamageSound.Play();
             }).AddTo(CompositeDisposable);
         }
 
---
+++ Assets/Script/Sound/SoundHolder.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public interface ISoundHolder : ISingleton
+{
+    AudioSource AttackSound { get; }
+    AudioSource MissSound { get; }
+    AudioSource DamageSound { get; }
+}
+
+public class SoundHolder : Singleton<SoundHolder, ISoundHolder>, ISoundHolder
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        PlayerLoopManager.Interface.GetInitEvent.Subscribe(_ => Initialize()).AddTo(this);
+    }
+
+    /// <summary>
+    /// インスタンス生成
+    /// </summary>
+    private void Initialize()
+    {
+        m_AttackSound = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.AttackSound).GetComponent<AudioSource>();
+        m_MissSound = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.MissSound).GetComponent<AudioSource>();
+        m_DamageSound = Instantiate(MasterDataHolder.Interface.CharacterMasterSetup.DamageSound).GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// 攻撃
+    /// </summary>
+    private AudioSource m_AttackSound;
+    AudioSource ISoundHolder.AttackSound => m_AttackSound;
+
+    /// <summary>
+    /// 空振り
+    /// </summary>
+    private AudioSource m_MissSound;
+    AudioSource ISoundHolder.MissSound => m_MissSound;
+
+    /// <summary>
+    /// 被ダメージ
+    /// </summary>
+    private AudioSource m_DamageSound;
+    AudioSource ISoundHolder.DamageSound => m_DamageSound;
+}
