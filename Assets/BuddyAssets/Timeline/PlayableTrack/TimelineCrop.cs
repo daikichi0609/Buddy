7a79eeab62c1baeeb158f42aea690b756c02ef58
東大貴 <nisisizema@icloud.com>
2023-10-26 08:11:43 +0900

    Timeline改修途中

---
+++ Assets/BuddyAssets/Timeline/PlayableTrack/TimelineCrop.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineCrop : PlayableBehaviour
+{
+    public float Duration { get; set; }
+
+    public string Text { get; set; }
+
+    //PlayableAsset(コマ)再生時実行される
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        // MessageBroker.Default.Publish(new DialogMessage(new DialogSetup.DialogPack(Duration, Text)));
+    }
+}
---
+++ Assets/BuddyAssets/Timeline/PlayableTrack/TimelineCropAsset.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineCropAsset : PlayableAsset
+{
+    [SerializeField]
+    private float m_Duration;
+
+    [SerializeField]
+    private string m_Text;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        //PlayableBehaviourを継承したTimelineCropクラスを元に、PlayableAsset(コマ)を作る
+        var player = ScriptPlayable<TimelineCrop>.Create(graph);
+
+        //TimelineCropクラスにあるプロパティを設定
+        var behaviour = player.GetBehaviour();
+        behaviour.Duration = m_Duration;
+        behaviour.Text = m_Text;
+
+        return player;
+    }
+}
